79db0a56243e608201fe0f3e5f8db4b297673028
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 09:21:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,6 +9,6 @@ def second_challenge
 
   #code your solution here!
 array_array = groceries.values 
-  concatenated = array_arrays.flatten
+  concatenated = array_array.flatten
 return concatenated
 end
